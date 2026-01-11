da8180d53f5524049387681d33438fb303def32b
priyankaflexadvertsing-rgb <priyankaflexadvertsing@gmail.com>
2026-01-11 19:01:02 +0530

    Update multer storage destination path

--- server/dist/shared/utils/multer.js
+++ server/dist/shared/utils/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 let count = 0;
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null, './oringinal_printing_files');
+        return cb(null, 'dist/oringinal_printing_files');
     },
     filename: function (req, file, cb) {
         return cb(null, `${++count}-${file.originalname}`);
